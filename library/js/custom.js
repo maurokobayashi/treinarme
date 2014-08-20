c8dd27472e5eb8dcb44e39da7819aa2b7d80a1a8
Mauro Kobayashi <mauro.kobayashi@gmail.com>
2014-08-20 15:31:59 -0300

    adding landing page to personal trainers

---
+++ library/js/custom.js
@@ -0,0 +1,20 @@
+howItWorks = function() {
+  $('html, body').animate({
+    scrollTop: $('#how-it-works').offset().top-84
+  }, 800);
+}
+findInstructor = function() {
+  $('html, body').animate({
+    scrollTop: $('#banner').offset().top-84
+  }, 800);
+}
+whyDoINeed = function() {
+  $('html, body').animate({
+    scrollTop: $('#why').offset().top-84
+  }, 800);
+}
+requestInvite = function() {
+  $('html, body').animate({
+    scrollTop: $('#request-invite').offset().top-84
+  }, 800);
+}
